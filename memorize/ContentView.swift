e923577be2f323c02044af69de5162fba29fc5fc
Cody Mercadante <78059143+cody909@users.noreply.github.com>
2022-04-12 15:25:26 -0700

    Added grid for displaying cards and add and delete functionality

--- memorize/ContentView.swift
+++ memorize/ContentView.swift
@@ -16,21 +16,63 @@ struct ContentView: View {
         animation: .default)
     private var items: FetchedResults<Item>
 
+    var emojis = ["🚂", "🚀", "🚁", "🚜"]
+    @State var emojiCount = 4
+    
     var body: some View {
-        HStack {
-            CardView()
-            CardView()
-            CardView()
-            CardView()
+        VStack {
+            
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji)
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+                .foregroundColor(.red)
+            }
+
+            
+            Spacer()
+            
+            HStack {
+                remove
+                Spacer()
+                add
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
         }
         .padding(.horizontal)
-        .foregroundColor(.red)
+    }
+    
+    var remove: some View {
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
+    }
+    
+    var add: some View {
+        Button(action: {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        }, label: {
+            Image(systemName: "plus.circle")
+
+        })
     }
 }
 
 struct CardView: View {
     @State var isFaceUp: Bool = true // creates a pointer to a variable and rebuild the view when a change in state is detected
-        
+    
+    var content: String
+    
     var body: some View {
         ZStack {
             let shape = RoundedRectangle(cornerRadius: 20)
@@ -39,8 +81,8 @@ struct CardView: View {
                     .fill()
                     .foregroundColor(.white)
                 shape
-                    .stroke(lineWidth: 3)
-                Text("✈️")
+                    .strokeBorder(lineWidth: 3)
+                Text(content)
                     .font(.largeTitle)
             } else {
                 shape
@@ -58,6 +100,7 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
             .preferredColorScheme(.dark)
+.previewInterfaceOrientation(.portrait)
         ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
             .preferredColorScheme(.light)
     }
