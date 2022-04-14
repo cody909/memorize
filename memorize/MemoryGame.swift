6aa52776652f1525cd6639bd6443dcc301cd9497
Cody Mercadante <78059143+cody909@users.noreply.github.com>
2022-04-14 15:58:38 -0700

    Added functionality for keeping score

--- memorize/MemoryGame.swift
+++ memorize/MemoryGame.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct MemoryGame<CardContent> where CardContent: Equatable {
     private(set) var cards: Array<Card>
-    
+    private(set) var score = 0
     private var indexOfTheOneAndOnlyFaceUpCard: Int?
     
     mutating func choose(_ card: Card) {
@@ -21,7 +21,14 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
                 if cards[chosenIndex].content == cards[potentialMatchIndex].content {
                     cards[chosenIndex].isMatched = true
                     cards[potentialMatchIndex].isMatched = true
+                    score += 2
+                } else {
+                    if cards[chosenIndex].seenBefore || cards[potentialMatchIndex].seenBefore {
+                        score -= 1
+                    }
                 }
+                cards[chosenIndex].seenBefore = true
+                cards[potentialMatchIndex].seenBefore = true
                 indexOfTheOneAndOnlyFaceUpCard = nil
             }  else {
                 for index in cards.indices {
@@ -40,11 +47,13 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
             cards.append(Card(content: content, id: pairIndex*2))
             cards.append(Card(content: content, id: pairIndex*2+1))
         }
+        cards = cards.shuffled()
     }
     
     struct Card: Identifiable {
         var isFaceUp: Bool = false
         var isMatched: Bool = false
+        var seenBefore: Bool = false
         var content: CardContent
         var id: Int
     }
