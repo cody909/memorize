27b5df7c31981f5d6013ffca2c61d630fc8befa7
Cody Mercadante <78059143+cody909@users.noreply.github.com>
2022-04-14 15:59:00 -0700

    Created a theme model

---
+++ memorize/Theme.swift
@@ -0,0 +1,24 @@
+//
+//  Theme.swift
+//  memorize
+//
+//  Created by Cody Mercadante on 4/14/22.
+//
+
+import Foundation
+
+struct EmojiMemoryGameTheme {
+    
+    struct Theme {
+        let name: String
+        let emojis: [String]
+        let color: String
+        let numberOfPairs: Int
+    }
+
+    static var themes: [Theme] = [
+        Theme(name: "Animals", emojis: ["🐶","🐯","🐸","🐝"], color: "green", numberOfPairs: 4),
+        Theme(name: "Sports", emojis: ["⚽️","🏀","🏈","⚾️"], color: "red", numberOfPairs: 3),
+        Theme(name: "Halloween", emojis: ["🕷","🕸","🎃","🦇"], color: "orange", numberOfPairs: 4)
+    ]
+}
