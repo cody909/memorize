b70eb2172ff13e703677fe252e84ef32c1e3a44d
Cody Mercadante <78059143+cody909@users.noreply.github.com>
2022-04-19 10:28:10 -0700

    made indexOfTheOneAndOnlyFaceUpCard a computed variable, refactored code, created extension for oneAndOnly on Array, changed card.content and card.id to constants

--- memorize/MemoryGame.swift
+++ memorize/MemoryGame.swift
@@ -9,39 +9,50 @@ import Foundation
 
 struct MemoryGame<CardContent> where CardContent: Equatable {
     private(set) var cards: Array<Card>
+    
     private(set) var score = 0
-    private var indexOfTheOneAndOnlyFaceUpCard: Int?
+    
+    private var indexOfTheOneAndOnlyFaceUpCard: Int? {
+        get { cards.indices.filter({ cards[$0].isFaceUp }).oneAndOnly }
+        set { cards.indices.forEach { cards[$0].isFaceUp = ($0 == newValue) } }
+                                
+    }
     
     mutating func choose(_ card: Card) {
         if let chosenIndex = cards.firstIndex(where: { $0.id == card.id}),
            !cards[chosenIndex].isFaceUp,
            !cards[chosenIndex].isMatched
         {
-            if let potentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard {
-                if cards[chosenIndex].content == cards[potentialMatchIndex].content {
-                    cards[chosenIndex].isMatched = true
-                    cards[potentialMatchIndex].isMatched = true
-                    score += 2
-                } else {
-                    if cards[chosenIndex].seenBefore || cards[potentialMatchIndex].seenBefore {
-                        score -= 1
-                    }
-                }
+            if let potentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard { // another card is already flipped
+                checkForMatch(chosenIndex, potentialMatchIndex)
                 cards[chosenIndex].seenBefore = true
                 cards[potentialMatchIndex].seenBefore = true
-                indexOfTheOneAndOnlyFaceUpCard = nil
-            }  else {
-                for index in cards.indices {
-                    cards[index].isFaceUp = false
-                }
+                cards[chosenIndex].isFaceUp = true
+            }  else { // no other card us flipped
                 indexOfTheOneAndOnlyFaceUpCard = chosenIndex
             }
-            cards[chosenIndex].isFaceUp.toggle()
+        }
+    }
+    
+    /// Checks is the content of two cards match and adds 2 to the score if they do;
+    /// else if the content does not match at leat one of the cards has been flipped before subtracts 1 from the score.
+    /// - Parameters:
+    ///   - firstCardIndex: the index of the first card turned over
+    ///   - secondCardIndex: the index of the second card turned over
+    private mutating func checkForMatch(_ firstCardIndex: Int, _ secondCardIndex: Int){
+        if cards[firstCardIndex].content == cards[secondCardIndex].content { // cards match
+            cards[firstCardIndex].isMatched = true
+            cards[secondCardIndex].isMatched = true
+            score += 2
+        } else { // cards do not match
+            if cards[firstCardIndex].seenBefore || cards[secondCardIndex].seenBefore {
+                score -= 1
+            }
         }
     }
     
     init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
-        cards = Array<Card>()
+        cards = []
         for pairIndex in 0..<numberOfPairsOfCards {
             let content = createCardContent(pairIndex)
             cards.append(Card(content: content, id: pairIndex*2))
@@ -51,11 +62,17 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
     }
     
     struct Card: Identifiable {
-        var isFaceUp: Bool = false
-        var isMatched: Bool = false
-        var seenBefore: Bool = false
-        var content: CardContent
-        var id: Int
+        var isFaceUp = false
+        var isMatched = false
+        var seenBefore = false
+        let content: CardContent
+        let id: Int
+    }
+}
+
+extension Array {
+    var oneAndOnly: Element? {
+        return count == 1 ? first : nil
     }
 }
 
